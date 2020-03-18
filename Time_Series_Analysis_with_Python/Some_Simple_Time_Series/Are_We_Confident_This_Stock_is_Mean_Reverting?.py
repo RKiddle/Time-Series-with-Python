0e682a5d2da754aa6482ae89cfecf3ba07eaeb29
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2020-03-18 18:50:00 +0700

    Update Are_We_Confident_This_Stock_is_Mean_Reverting?.py

--- Time_Series_Analysis_with_Python/Some_Simple_Time_Series/Are_We_Confident_This_Stock_is_Mean_Reverting?.py
+++ Time_Series_Analysis_with_Python/Some_Simple_Time_Series/Are_We_Confident_This_Stock_is_Mean_Reverting?.py
@@ -3,16 +3,16 @@ from statsmodels.graphics.tsaplots import plot_acf
 from math import sqrt
 
 # Compute and print the autocorrelation of MSFT weekly returns
-autocorrelation = returns['Adj Close'].acf()
+autocorrelation = returns['Adj Close'].autocorr()
 print("The autocorrelation of weekly MSFT returns is %4.2f" %(autocorrelation))
 
 # Find the number of observations by taking the length of the returns DataFrame
-nobs = ___
+nobs = len(returns)
 
 # Compute the approximate confidence interval
-conf = 1.96/___
+conf = 1.96/sqrt(nobs)
 print("The approximate confidence interval is +/- %4.2f" %(conf))
 
 # Plot the autocorrelation function with 95% confidence intervals and 20 lags using plot_acf
-plot_acf(___, alpha=0.05, ___)
+plot_acf(returns, alpha=0.05, lags=20)
 plt.show()
