9d0eeab06709736d239763a2344981b2e0734417
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2020-03-18 16:37:51 +0700

    Update Flying_Saucers_Aren't_Correlated_to_Flying_Markets.py

--- Time_Series_Analysis_with_Python/Correlation_and_Autocorrelation/Flying_Saucers_Aren't_Correlated_to_Flying_Markets.py
+++ Time_Series_Analysis_with_Python/Correlation_and_Autocorrelation/Flying_Saucers_Aren't_Correlated_to_Flying_Markets.py
@@ -4,5 +4,5 @@ print("Correlation of levels: ", correlation1)
 
 # Compute correlation of percent changes
 changes = levels.pct_change()
-correlation2 = changes.DJI.corr(levels.UFO)
+correlation2 = changes.DJI.corr(changes.UFO)
 print("Correlation of changes: ", correlation2)
