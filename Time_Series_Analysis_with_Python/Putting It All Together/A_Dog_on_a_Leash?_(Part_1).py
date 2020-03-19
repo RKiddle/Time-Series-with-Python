12a2e85a54cd642f7fe1028e457d2b8e2bef207e
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2020-03-19 17:09:46 +0700

    Create A_Dog_on_a_Leash?_(Part_1).py

---
+++ Time_Series_Analysis_with_Python/Putting It All Together/A_Dog_on_a_Leash?_(Part_1).py
@@ -0,0 +1,13 @@
+# Plot the prices separately
+plt.subplot(2,1,1)
+#multiply by the HO price by 7.25 to match the units of NG
+plt.plot(7.25*HO, label='Heating Oil')
+plt.plot(NG, label='Natural Gas')
+plt.legend(loc='best', fontsize='small')
+
+# Plot the spread
+plt.subplot(2,1,2)
+plt.plot(7.25*HO-NG, label='Spread')
+plt.legend(loc='best', fontsize='small')
+plt.axhline(y=0, linestyle='--', color='k')
+plt.show()
