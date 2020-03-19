2f0714f361c9d080436de8d2058a29aa1ffc3385
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2020-03-19 17:16:24 +0700

    Create A_Dog_on_a_Leash?_(Part_2).py

---
+++ Time_Series_Analysis_with_Python/Putting It All Together/A_Dog_on_a_Leash?_(Part_2).py
@@ -0,0 +1,13 @@
+# Import the adfuller module from statsmodels
+from statsmodels.tsa.stattools import adfuller
+
+# Compute the ADF for HO and NG
+# The argument for adfuller must be a series, so you need to include the column 'Close'
+result_HO = adfuller(HO['Close'])
+print("The p-value for the ADF test on HO is ", result_HO[1])
+result_NG = adfuller(NG['Close'])
+print("The p-value for the ADF test on NG is ", result_NG[1])
+
+# Compute the ADF of the spread
+result_spread = adfuller(7.25 * HO['Close'] - NG['Close'])
+print("The p-value for the ADF test on the spread is ", result_spread[1])
