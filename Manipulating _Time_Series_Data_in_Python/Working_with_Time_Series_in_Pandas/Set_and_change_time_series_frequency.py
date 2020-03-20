4b42e2dd0201739485f04cb9ea2c292631a23284
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2020-03-20 20:47:32 +0700

    Create Set_and_change_time_series_frequency.py

---
+++ Manipulating _Time_Series_Data_in_Python/Working_with_Time_Series_in_Pandas/Set_and_change_time_series_frequency.py
@@ -0,0 +1,18 @@
+# Inspect data
+print(co.info())
+
+# Set the frequency to calendar daily
+co = co.asfreq('D')
+
+# Plot the data
+co.plot(subplots=True)
+plt.show()
+
+
+# Set frequency to monthly
+co = co.asfreq('M')
+
+# Plot the data
+co.plot(subplots=True)
+plt.show()
+
