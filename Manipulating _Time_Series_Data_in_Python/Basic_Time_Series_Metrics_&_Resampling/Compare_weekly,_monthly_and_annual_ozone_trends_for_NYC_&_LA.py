3938b489cc0fd55c8aa40bc0953f20b0f14a0a01
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2020-03-20 21:03:19 +0700

    Create Compare_weekly,_monthly_and_annual_ozone_trends_for_NYC_&_LA.py

---
+++ Manipulating _Time_Series_Data_in_Python/Basic_Time_Series_Metrics_&_Resampling/Compare_weekly,_monthly_and_annual_ozone_trends_for_NYC_&_LA.py
@@ -0,0 +1,20 @@
+# Import and inspect data here
+ozone = pd.read_csv('ozone.csv', parse_dates=['date'], index_col='date')
+
+print(ozone.info())
+
+# Calculate and plot the weekly average ozone trend
+ozone.resample('W').mean().plot()
+plt.show()
+
+# Calculate and plot the monthly average ozone trend
+ozone.resample('M').mean().plot()
+plt.show()
+
+
+
+# Calculate and plot the annual average ozone trend
+ozone.resample('A').mean().plot()
+plt.show()
+
+
