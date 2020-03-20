a8b0b7b099feba6b64646cdbd77526eeb39618a2
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2020-03-20 20:48:24 +0700

    Create Shifting_stock_prices_across_time.py

---
+++ Manipulating _Time_Series_Data_in_Python/Working_with_Time_Series_in_Pandas/Shifting_stock_prices_across_time.py
@@ -0,0 +1,14 @@
+# Import data here
+google = pd.read_csv('google.csv', parse_dates=['Date'], index_col='Date') 
+
+# Set data frequency to business daily
+google = google.asfreq('B')
+
+# Create 'lagged' and 'shifted'
+google['shifted'] = google.Close.shift(periods=90) 
+google['lagged'] = google.Close.shift(periods=-90) 
+
+# Plot the google price series
+google.plot()
+plt.show()
+
