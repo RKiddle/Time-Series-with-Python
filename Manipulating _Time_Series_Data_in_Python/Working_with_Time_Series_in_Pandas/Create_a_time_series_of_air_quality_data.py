4aa1e5853b86739f8342ce52de35f93f28677116
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2020-03-20 20:45:38 +0700

    Create Create_a_time_series_of_air_quality_data.py

---
+++ Manipulating _Time_Series_Data_in_Python/Working_with_Time_Series_in_Pandas/Create_a_time_series_of_air_quality_data.py
@@ -0,0 +1,16 @@
+# Inspect data
+print(data.info())
+
+# Convert the date column to datetime64
+data.date = pd.to_datetime(data.date)
+
+# Set date column as index
+data.set_index('date', inplace=True) 
+
+# Inspect data 
+print(data.info())
+
+# Plot data
+data.plot(subplots=True)
+plt.show()
+
