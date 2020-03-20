b93c2a2322c86966cff4cb2829742230cdd774fe
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2020-03-20 21:01:09 +0700

    Create Use_interpolation_to_create_weekly_employment_data.py

---
+++ Manipulating _Time_Series_Data_in_Python/Basic_Time_Series_Metrics_&_Resampling/Use_interpolation_to_create_weekly_employment_data.py
@@ -0,0 +1,16 @@
+# Inspect data here
+print(monthly.info())
+
+# Create weekly dates
+weekly_dates = pd.date_range(start=monthly.index.min(), end=monthly.index.max(), freq='W')
+
+# Reindex monthly to weekly data
+weekly = monthly.reindex(weekly_dates)
+
+# Create ffill and interpolated columns
+weekly['ffill'] = weekly.UNRATE.ffill()
+weekly['interpolated'] = weekly.UNRATE.interpolate()
+
+# Plot weekly
+weekly.plot()
+plt.show()
