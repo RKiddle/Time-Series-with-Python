8c4d8e964f8b9f6e5e502a382fd04020193ab179
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2020-03-22 20:51:58 +0700

    Create Compute_numerical_summaries.py

---
+++ visualizing_time_series_data_in_python/Summary_Statistics_and_Diagnostics/Compute_numerical_summaries.py
@@ -0,0 +1,8 @@
+# Print out summary statistics of the co2_levels DataFrame
+print(co2_levels.describe())
+
+# Print out the minima of the co2 column in the co2_levels DataFrame
+print(co2_levels.co2.min())
+
+# Print out the maxima of the co2 column in the co2_levels DataFrame
+print(co2_levels.co2.max())
