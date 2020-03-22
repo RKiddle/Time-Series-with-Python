7b2459314cae118f1fbb8508a592adb27aea42d5
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2020-03-22 20:21:53 +0700

    Update Find_missing_values.py

--- visualizing_time_series_data_in_python/Summary_Statistics_and_Diagnostics/Find_missing_values.py
+++ visualizing_time_series_data_in_python/Summary_Statistics_and_Diagnostics/Find_missing_values.py
@@ -1,2 +1,8 @@
 # Display first seven rows of co2_levels
-print(___.____(____))
+print(co2_levels.head(7))
+
+# Set datestamp column as index
+co2_levels = co2_levels.set_index('datestamp')
+
+# Print out the number of missing values
+print(co2_levels.isnull().sum())
